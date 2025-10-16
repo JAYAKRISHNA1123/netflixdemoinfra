ff8135673bf1f038e5462fb3b89bde6bb6246af2
JAYAKRISHNA1123 <jayakrishna34873@gmail.com>
2025-10-16 08:46:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-0341d95f75f311023"
 instance_type = "t2.medium"
-key_name = "swiggykeypair"
+key_name = "DEMOKEYPAIR"
 vpc_security_group_ids = ["sg-0882bf5acd0062400"]
 tags = {
 Name = var.instance_names[count.index]
